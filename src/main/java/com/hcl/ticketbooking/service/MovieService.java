6d74a2e38d9a3ce29bd5cc2f155a9e1c224f0eb5
= <siva3060@gmail.com>
2019-08-23 15:36:42 +0530

    list of movies

---
+++ src/main/java/com/hcl/ticketbooking/service/MovieService.java
@@ -0,0 +1,11 @@
+package com.hcl.ticketbooking.service;
+
+import java.util.List;
+
+import com.hcl.ticketbooking.dto.MovieDTO;
+
+public interface MovieService {
+	
+	public List<MovieDTO> movieList();
+
+}
---
+++ src/main/java/com/hcl/ticketbooking/service/MovieServiceImpl.java
@@ -0,0 +1,40 @@
+package com.hcl.ticketbooking.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.ticketbooking.dto.MovieDTO;
+import com.hcl.ticketbooking.entity.Movie;
+import com.hcl.ticketbooking.repository.MovieRepository;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+	@Autowired
+	MovieRepository movieRepository;
+
+	public List<MovieDTO> movieList() {
+
+		List<MovieDTO> movieDTOList = new ArrayList<>();
+
+		List<Movie> movieList = movieRepository.findAll();
+
+		for (Movie movie : movieList) {
+
+			MovieDTO movieDTO = new MovieDTO();
+			movieDTO.setMovieId(movie.getMovieId());
+			movieDTO.setMovieName(movie.getMovieName());
+			/*
+			 * movieDTO.setMovieLanguage(movie.getMovieLanguage());
+			 * movieDTO.setMovieRunningTime(movie.getMovieRunningTime());
+			 */ movieDTOList.add(movieDTO);
+
+		}
+
+		return movieDTOList;
+	}
+
+}
